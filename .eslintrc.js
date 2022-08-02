155fdb85e4504ae4a5a5f4fe106315baa02f2225
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-08-02 17:51:14 +0500

    prettier-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,30 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "error",
+    // indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    // quotes: ["error", "double"],
+    // semi: ["error", "always"],
+  },
 };
