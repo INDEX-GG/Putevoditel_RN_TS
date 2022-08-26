015a203f5b82cb74d06dbc8ffc1b580a4ef88096
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-08-26 13:57:49 +0500

    pod-clear

--- src/hooks/useDownloadFile.ts
+++ src/hooks/useDownloadFile.ts
@@ -1,25 +1,28 @@
 import RNFetchBlob from "rn-fetch-blob";
+import RNFS from "react-native-fs";
+import { Platform } from "react-native";
 
 export const useDownloadFile = () => {
   const handleDownloadFile = (fileName = "file") => {
-    const { config, fs } = RNFetchBlob;
-    const directory = fs.dirs.DownloadDir;
-    console.log(fs.dirs.DocumentDir);
-    const options = {
-      fileCache: true,
-      addAndroidDownloads: {
-        useDownloadManager: true,
-        notification: true,
-        path: `${directory}/${fileName}.docx`,
-        description: `Файл скачан из приложения "Услугив кормане"`,
-      },
-    };
-
-    config(options)
-      .fetch("GET", "https://docxtemplater.com/docs/simple.docx")
-      .then((res) => {
-        // console.log(res);
-      });
+    console.log(RNFS);
+    // const { config, fs } = RNFetchBlob;
+    // const directory = fs.dirs.DownloadDir;
+    // console.log(fs.dirs.DocumentDir, Platform.OS, RNFS);
+    // const options = {
+    //   fileCache: true,
+    //   addAndroidDownloads: {
+    //     useDownloadManager: true,
+    //     notification: true,
+    //     path: `${directory}/${fileName}.docx`,
+    //     description: `Файл скачан из приложения "Услугив кормане"`,
+    //   },
+    // };
+    //
+    // config(options)
+    //   .fetch("GET", "https://docxtemplater.com/docs/simple.docx")
+    //   .then((res) => {
+    //     // console.log(res);
+    //   });
   };
 
   return {
